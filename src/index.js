785c4a8322b544cee5c77ff8db4d79be8a4414e6
barsu5489 <ebarsulai@gmail.com>
2022-12-24 15:05:53 +0300

    rendered dog and dog image

--- src/index.js
+++ src/index.js
@@ -9,10 +9,23 @@ function fetchPup(){
 
 function renderPup(pup){
     const dogBar = document.getElementById('dog-bar')
+    const dogInfo = document.getElementById('dog-info')
     pup.forEach(pup=>{
         const span =document.createElement('span')
         span.textContent = pup.name
         dogBar.appendChild(span)
+
+        const img = document.createElement('img')
+        img.src = `${pup.image}`
+
+        const h2 = document.createElement('h2')
+        h2.textContent = pup.name
+
+        const btn = document.createElement('button')
+        btn.textContent = 'Good Dog!'
+        dogInfo.appendChild(img)
+        dogInfo.appendChild(h2)
+        dogInfo.appendChild(btn)
     })
   
 }
